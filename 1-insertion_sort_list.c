f1e31e62f45ba2b0c50b339ae724c11459cd7c08
“Tristian <tristian.davis@atlasschool.com>
2024-08-10 16:14:02 -0500

    removing lines for betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,20 +1,16 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list - sorts list of ints
  * @list: doubly linked list
  * Return: void
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *ptr;
 	listint_t *tmp;
 
 	if (!list)	/*if null*/
-	{
 		return;
-	}
 	ptr = *list;
 	while (ptr)
 	{
@@ -23,28 +19,16 @@ void insertion_sort_list(listint_t **list)
 			tmp = ptr->next; /* store the next node*/
 			ptr->next = tmp->next;
 			tmp->prev = ptr->prev;
-
 			if (ptr->prev)	/*update prev*/
-			{
 				ptr->prev->next = tmp;
-			}
 			if (tmp->next)	/*update next*/
-			{
 				tmp->next->prev = ptr;
-			}
-
 			ptr->prev = tmp;	/*swap nodes*/
 			tmp->next = ptr;
-
 			if (tmp->prev)
-			{
 				ptr = tmp->prev;
-			}
 			else
-			{
 				*list = tmp;	/*update head*/
-			}
-
 			print_list(*list);
 		}
 		ptr = ptr->next;
